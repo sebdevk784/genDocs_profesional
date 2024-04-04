647adb8c8fbdbd0d6130b4406c8bc8aee6b66f07
sebdevk784 <165195544+sebdevk784@users.noreply.github.com>
2024-04-03 20:16:54 -0500

    actualización estructura pagina
    *se crea el archivo hoja_vida.php que contiene el formulario que anteriormente estaba en index.php cuyo fin reestructurar el sitio web y su alcance a futuro
    *en el index.php se deberá añadir opciones y guías de usuario

--- index.php
+++ index.php
@@ -1,145 +1,23 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
-  <link rel="stylesheet" href="./recursos/estilos.css">
-  <meta charset="UTF-8" />
-  <title>hoja de vida</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <header>
-  <?php
-  include "./navbar_template.php";
-  ?>
+    <?php
+    include "./navbar_template.php";
+    ?>
 </header>
+<!--Insertar documentación de l sitio web, iconos para seleccionar crear hoja de vida, carta de renuncia 
+o recomendacion.
+Insertar texto y videos del funcionamiento del sitio web para que sea mas claro para un usuario el cómo navegar
+por aca :)-->
 
 <body>
-  <!--Scrip para el formulario
-  la funcion agregar_referencias sirve para añadir mas divs en caso de que el usuario desee agregar mas referencias.
-  recibe el parametro idContenedor, es decir, el contenedor que se duplicará en caso de ser requerido
 
-  la funcion eliminar referencias sirve para eliminar un contenedor creado por la funcion anterior, manteniendo el 
-  contenedor original
-  recibe como parametro idContenedor, es decir, el contenedor que se deberá eliminar en caso de ser requerido
--->
-  <script>
-    //crear campo de referencia
-    function agregar_referencias(idContenedor) {
-      var contenedor = document.getElementById(idContenedor);
-      //clonar los campos de referencia
-      var ultimos_inputs_contenedor = contenedor.lastElementChild;
-      var clon_contenedor = ultimos_inputs_contenedor.cloneNode(true);
-      //limpiar valor de los inputs clonados
-      var inputs_contenedor = clon_contenedor.getElementsByTagName('input');
-      for (var i = 0; i < inputs_contenedor.length; i++) {
-        inputs_contenedor[i].value = '';
-      }
-
-      //añadir el clon
-      contenedor.appendChild(clon_contenedor);
-
-    }
-    //funcion para eliminar divs
-    function eliminar_referencias(idContenedor) {
-      var contenedor = document.getElementById(idContenedor);
-      if (contenedor.children.length > 1) {
-        contenedor.removeChild(contenedor.lastElementChild);
-      }
-    }
-  </script>
-  <div class="formulario">
-    <form action="crear_cv_pdf.php" method="POST" enctype="multipart/form-data">
-      <!--seccion datos personales -->
-      <label>DATOS PERSONALES.</label><br>
-      <label>Nombre:</label>
-      <input type="text" name="nombre" placeholder="ingresa nombre completo">
-      <label>Foto:</label>
-      <input type="file" name="foto">
-      <label>Documento de identidad:</label>
-      <input type="text" name="documento_identidad" placeholder="ingresa solo el numero de documento">
-      <label>Fecha de nacimiento:</label>
-      <input type="date" name="fecha_nacimiento">
-      <label>Estado civil:</label>
-      <input type="text" name="estado_civil" placeholder="soltero/casado/viudo/etc">
-      <label>Dirección:</label>
-      <input type="text" name="direccion" placeholder="solo direccion y numero">
-      <label>Barrio:</label>
-      <input type="text" name="barrio" placeholder="solo el nombre del barrio">
-      <label>Celular:</label>
-      <input type="text" name="celular">
-      <label>Correo:</label>
-      <input type="text" name="correo" placeholder="ingresa el correo personal">
-      <!--seccion formacion academica -->
-      <div id="formacion_academica">
-        <div id="grupo_formacion_academica" class="div_grupo_auxiliar">
-          <label>FORMACION ACADEMICA.</label><br>
-          <label>Nombre de la institución:</label>
-          <input type="text" name="instituciones[]">
-          <label>Titulo obtenido:</label>
-          <input type="text" name="titulo_obtenido[]" placeholder="primaria/secundaria/otro">
-          <label>Año:</label>
-          <input type="text" name="año_graduacion[]" placeholder="ingresa el año de graduación">
-        </div>
-      </div>
-      <button type="button" onclick="agregar_referencias('formacion_academica')">agregar formación acádemica</button>
-      <button type="button" onclick="eliminar_referencias('formacion_academica')">eliminar formación acádemica</button>
-      <!--seccion experiencia laboral -->
-      <div id="experiencia_laboral">
-        <div id="grupo_experiencia_laboral" class="div_grupo_auxiliar">
-          <label>EXPERIENCIA LABORAL.</label><br>
-          <label>Nombre de la empresa:</label>
-          <input type="text" name="nombre_empresa[]">
-          <label>Cargo ocupado:</label>
-          <input type="text" name="cargo_ocupado[]">
-          <label>Nombre del jefe:</label>
-          <input type="text" name="nombre_jefe[]">
-          <label>Telefono:</label>
-          <input type="text" name="telefono_empresa[]" placeholder="numero del telefono de la empresa">
-          <label>Duración en la empresa:</label>
-          <input type="text" name="numero_duracion_empresa[]">
-          <select name="unidad_duracion_empresa[]">
-            <option value="año">Año</option>
-            <option value="años">Años</option>
-            <option value="mes">Mes</option>
-            <option value="meses">Meses</option>
-          </select>
-        </div>
-      </div>
-      <button type="button" onclick="agregar_referencias('experiencia_laboral')">agregar experiencia</button>
-      <button type="button" onclick="eliminar_referencias('experiencia_laboral')">eliminar experiencia</button>
-      <!--seccion referencias familiares -->
-      <div id="referencias_familiares">
-        <div id="grupo_referencia_familiar" class="div_grupo_auxiliar">
-          <label>REFERENCIAS FAMILIARES.</label><br>
-          <label>Nombre de la referencia:</label>
-          <input type="text" name="nombre_referencia_familiar[]">
-          <label>Parentesco de la referencia:</label>
-          <input type="text" name="parentesco_referencia_familiar[]" placeholder="mamá/papá/hermano/tío/etc.">
-          <label>Ocupacion de la referencia:</label>
-          <input type="text" name="ocupacion_referencia_familiar[]">
-          <label>Celular de la referencia:</label>
-          <input type="text" name="celular_referencia_familiar[]">
-        </div>
-      </div>
-      <button type="button" onclick="agregar_referencias('referencias_familiares')">agregar referencia familiar</button>
-      <button type="button" onclick="eliminar_referencias('referencias_familiares')">eliminar referencia familiar</button>
-      <!--seccion referencias personales -->
-      <div id="referencias_personales">
-        <div id="grupo_referencia_personal" class="div_grupo_auxiliar">
-          <label>REFERENCIAS PERSONAL.</label><br>
-          <label>Nombre de la referencia:</label>
-          <input type="text" name="nombre_referencia_personal[]">
-          <label>Ocupacion de la referencia:</label>
-          <input type="text" name="ocupacion_referencia_personal[]">
-          <label>Celular de la referencia:</label>
-          <input type="text" name="celular_referencia_personal[]">
-        </div>
-      </div>
-      <button type="button" onclick="agregar_referencias('referencias_personales')">agregar referencia personal</button>
-      <button type="button" onclick="eliminar_referencias('referencias_personales')">eliminar referencia personal</button>
-      <!--enviar formulario -->
-      <input type="submit" value="enviar">
-    </form>
-  </div>
 </body>
 
 </html>
